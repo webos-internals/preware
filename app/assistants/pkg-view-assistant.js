61fd75845296b30faf97ffe26248ed8ea239c444
oil <brandonvb@gmail.com>
2009-10-10 06:51:48 -0700

    removed non-code accidentially typed :/

--- app/assistants/pkg-view-assistant.js
+++ app/assistants/pkg-view-assistant.js
@@ -129,7 +129,7 @@ PkgViewAssistant.prototype.setupImages = function()
 				Mojo.Event.listen(this.controller.get('app_' + this.dependents[d]), Mojo.Event.tap, this.appTap);
 				packages.packages[this.dependents[d]].iconFill(this.controller.get('app_' + this.dependents[d]));
 			}
-		}lol
+		}
 		else
 		{
 			this.controller.get('scrollerContainer').style.display = 'none';
