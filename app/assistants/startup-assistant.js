0c89f90dcfd61e68422c66e8cf2b7d17351bffe1
Rod Whitby <rod@whitby.id.au>
2010-03-06 00:23:53 +1030

    Show the donation message on each install

--- app/assistants/startup-assistant.js
+++ app/assistants/startup-assistant.js
@@ -3,6 +3,8 @@ function StartupAssistant()
 	// on first start, this message is displayed, along with the current version message from below
 	this.firstMessage = $L('Here are some tips for first-timers:<ul><li>Preware will take some time to download the data for all your enabled package feeds</li><li>Select the "Preferences" menu item to change how often the feeds are updated</li><li>Customize the main screen using the "Show Available Types" preference options</li><li>Select the "Manage Feeds" menu item to enable just your desired package feeds</li><li>To search, just start typing in the "List of Everything"</li></ul>We hope you enjoy all of the many Applications, Themes and Patches that Preware makes available.<br>Please consider making a <a href=http://www.webos-internals.org/wiki/WebOS_Internals:Site_support>donation</a> if you wish to show your appreciation.');
 	
+	this.secondMessage = $L('We hope you enjoy all of the many Applications, Themes and Patches that Preware makes available.<br>Please consider making a <a href=http://www.webos-internals.org/wiki/WebOS_Internals:Site_support>donation</a> if you wish to show your appreciation.');
+	
 	// on new version start
 	this.newMessages =
 	[
@@ -10,6 +12,7 @@ function StartupAssistant()
 			version: '0.9.30',
 			log:
 			[
+				'Added the ability to create new feeds and delete existing feeds',
 				'Now chooses the correct information to display for packages in multiple feeds',
 				'Fixes the bug in the only show free applications preference'
 			]
@@ -263,6 +266,7 @@ StartupAssistant.prototype.setup = function()
 	}
 	if (vers.isNew)
 	{
+		html += '<div class="text">' + this.secondMessage + '</div>';
 		for (var m = 0; m < this.newMessages.length; m++)
 		{
 			html += Mojo.View.render({object: {title: 'v' + this.newMessages[m].version}, template: 'startup/changeLog'});
