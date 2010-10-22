05d8bece3b471c9116d6088a94ba2c2ee0fcfa7d
oil <brandonvb@gmail.com>
2010-10-22 01:03:56 -0700

    updated donate link

--- app/assistants/main-assistant.js
+++ app/assistants/main-assistant.js
@@ -8,7 +8,7 @@ function MainAssistant()
 		{weight: 15, text: $L("The Open Standard Installer")},
 		{weight: 15, text: $L("The Universal Application Installer")},
 		{weight: 15, text: $L("Accessing All Open Standard Feeds")},
-		{weight:  6, text: $L("<a href=\"http://www.webos-internals.org/wiki/WebOS_Internals:Site_support\">Donated</a> To WebOS Internals Lately?")},
+		{weight:  6, text: $L("<a href=\"http://donate.webos-internals.org/\">Donated</a> To WebOS Internals Lately?")},
 		{weight:  4, text: $L("The w Isn't Capitalized")},
 		{weight:  2, text: $L("Random Taglines Are Awesome")},
 		{weight:  2, text: $L("We Know Palm Loves Preware")},
