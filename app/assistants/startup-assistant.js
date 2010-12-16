77b4608a7729953df856042031b032f6948e9363
Rod Whitby <rod@whitby.id.au>
2010-12-16 15:50:00 +1030

    Better compatibility with unknown future webOS versions

--- app/assistants/startup-assistant.js
+++ app/assistants/startup-assistant.js
@@ -10,6 +10,7 @@ function StartupAssistant(changelog)
     // on new version start
     this.newMessages =
 	[
+	 {	 version: '1.4.9', log: [ 'Better compatibility with unknown future webOS versions' ] },
 	 {	 version: '1.4.8', log: [ 'Now calls the correct software manager updates screen on webOS 2.0' ] },
 	 {	 version: '1.4.7', log: [ 'Fixed update, install and remove status messages on webOS 2.0' ] },
 	 {	 version: '1.4.6', log: [ 'New "App Catalog" theme preferences thanks to Garrett92C',
