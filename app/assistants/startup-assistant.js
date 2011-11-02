da06657f46a4cf0209940c087999509ccb3de9e3
Rod Whitby <rod@whitby.id.au>
2011-11-03 09:51:53 +1030

    Fixed missing package information bug

--- app/assistants/startup-assistant.js
+++ app/assistants/startup-assistant.js
@@ -10,6 +10,7 @@ function StartupAssistant(changelog)
     // on new version start
     this.newMessages =
 	[
+	 {	 version: '1.8.5', log: [ 'Fixed missing package information bug' ] },
 	 {	 version: '1.8.4', log: [ 'Added some additional Second Line preference options' ] },
 	 {	 version: '1.8.3', log: [ 'Please read http://testing.preware.org/ if you use the testing feeds' ] },
 	 {	 version: '1.8.2', log: [ 'Fixed the removal of obsolete testing feeds' ] },
