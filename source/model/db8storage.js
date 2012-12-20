2754c473ff31e48f6d4beba040545d63a891150c
Garfonso <achim@tratschtante.de>
2012-12-20 03:27:46 +0100

    can't use members during component initialization.

--- source/model/db8storage.js
+++ source/model/db8storage.js
@@ -5,21 +5,22 @@ enyo.kind({
   //TODO: see how to get that over from enyo1 / old webos to open webos or if this is already..
 });
 
+var preware2Domain = "org.webosinternals.preware2";
+
 //TODO: uncomment this.$.XYZ.call lines if DbService is implemented..
 enyo.singleton({
   name: "preware.db8Storage",
-  domain: "org.webosinternals.preware2",
   events: {
     onDeleteAllFinished: "" //will set inEvent.success to false or true.
   },
   kinds: [ {
               //id is set in DbService kind.
-             owner: this.domain,
+             owner: preware2Domain,
              indexes: [
                { name: "display", props: [ {name: "display", type: "single", tokenize: "all", collate: "primary"}] }
              ] } ],
   components: [
-    {kind: "DbService", dbKind: this.domain + ".justType:1", onFailure: "dbFailure", onSuccess: "dbSuccess", components: [
+    {kind: "DbService", dbKind: preware2Domain + ".justType:1", onFailure: "dbFailure", onSuccess: "dbSuccess", components: [
       {name: "putOneKind", method: "putKind"},
       {name: "putPermission", method: "putPermissions"},
       {name: "putData", method: "put"},
@@ -39,14 +40,14 @@ enyo.singleton({
     }
   },
   setupJustType: function() {
-    var permObj = [{"type":"db.kind","object":this.domain +".justType:1", "caller":"com.palm.launcher", "operations":{"read":"allow"}}];
+    var permObj = [{"type":"db.kind","object":preware2Domain +".justType:1", "caller":"com.palm.launcher", "operations":{"read":"allow"}}];
     //this.$.putPermission.call({permissions: permObj});
   },
   putArray: function(array) {
     this.$.putData.call({objects: array});
   },
   deleteAll: function() {
-    //this.$.deleteAllData.call({query: {from: this.domain + ".justType:1"}});
+    //this.$.deleteAllData.call({query: {from: preware2Domain + ".justType:1"}});
     //TODO: remove this, if DbService is implemented!!!
     enyo.error("faking delete all success!");
     this.doDeleteAll({success: false});
