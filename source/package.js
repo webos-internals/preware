b0174aebb549384c53531f6ae80c532f8b25e625
Garfonso <achim@tratschtante.de>
2012-12-23 02:50:27 +0100

    removed webos-ext, now using phonegap.

--- source/package.js
+++ source/package.js
@@ -1,7 +1,7 @@
 enyo.depends(
 	"$lib/layout",
 	"$lib/onyx",	// To theme Onyx using Theme.less, change this line to $lib/onyx/source,
-  "$lib/webos-ext", //adds eyno.webOS and enyo.webOS.ServiceRequest. Maybe that will not be needed on the final device?
+  //"$lib/webos-ext", //adds eyno.webOS and enyo.webOS.ServiceRequest. Maybe that will not be needed on the final device?
 	//"Theme.less",	// uncomment this line, and follow the steps described in Theme.less
 	"App.css",
 	"App.js",
