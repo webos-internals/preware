dd24fd8e34aff8679b466d676160ed363ccc6a12
josh <jpalmerwatkins@me.com>
2013-01-13 23:36:13 +0000

    Preware: Remove webOS-ext
    Open-WebOS-DCO-1.0-Signed-Off-By: Josh Palmer <jpalmerwatkins@me.com>

--- enyo2/source/package.js
+++ enyo2/source/package.js
@@ -3,7 +3,6 @@ enyo.depends(
 	"$lib/onyx",	// To theme Onyx using Theme.less, change this line to $lib/onyx/source,
 	//"Theme.less",	// uncomment this line, and follow the steps described in Theme.less
 	"$lib/webos-ports-lib",
-	//"$lib/webos-ext", //adds enyo.webOS and enyo.webOS.ServiceRequest. Maybe that will not be needed on the final device?
 	"App.css",
 	"App.js",
 	"model"
