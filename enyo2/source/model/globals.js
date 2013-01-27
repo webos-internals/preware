3352b905e80e21ecc287982518b3bf7b0f00f843
Garfonso <achim@tratschtante.de>
2013-01-27 21:15:39 +0100

    added "include" function to string object
    this is taken from prototype library and used in PackagesModel and some
    other files. Where to put that? Or how to best replace it?

--- enyo2/source/model/globals.js
+++ enyo2/source/model/globals.js
@@ -1,6 +1,11 @@
 //TODO: we maybe want to get rid of this.. right now I have no idea how to get the data that I configure here dynamically.
 /*global enyo */
 
+//TODO: move this somewhere more appropriate.
+String.prototype.include = function (pattern) {
+	return this.indexOf(pattern) > -1;
+}
+
 enyo.singleton({
 	name: "preware.Globals",
 	published: {
