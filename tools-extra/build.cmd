fe99989500d2aba273523e7d1a9cbf2d20686fdb
Garfonso <achim@tratschtante.de>
2013-06-04 22:41:56 +0200

    some build stuff (windows)

--- tools-extra/build.cmd
+++ tools-extra/build.cmd
@@ -1,9 +1,9 @@
-rmdir /S /Q phonegap-phonegap-2.2.0\lib\webos\framework
-mkdir phonegap-phonegap-2.2.0\lib\webos\framework
+rmdir /S /Q ..\phonegap-phonegap-2.2.0\lib\webos\framework
+mkdir ..\phonegap-phonegap-2.2.0\lib\webos\framework
 
-call tools\deploy.bat
-copy appinfo.json deploy\preware-enyo2\
-xcopy /E /Y deploy\preware-enyo2\* phonegap-phonegap-2.2.0\lib\webos\framework
-copy phonegap-phonegap-2.2.0\lib\webos\lib\cordova.webos.js phonegap-phonegap-2.2.0\lib\webos\framework\cordova-2.2.0.js
-call palm-package phonegap-phonegap-2.2.0\lib\webos\framework
-call palm-install *.ipk
+call ..\tools\deploy.bat
+copy ..\appinfo.json ..\deploy\preware-enyo2\
+xcopy /E /Y ..\deploy\preware-enyo2\* ..\phonegap-phonegap-2.2.0\lib\webos\framework
+copy ..\phonegap-phonegap-2.2.0\lib\webos\lib\cordova.webos.js ..\phonegap-phonegap-2.2.0\lib\webos\framework\cordova-2.2.0.js
+call palm-package ..\phonegap-phonegap-2.2.0\lib\webos\framework
+call palm-install ..\*.ipk
--- tools-extra/build_debug.cmd
+++ tools-extra/build_debug.cmd
@@ -1,14 +1,15 @@
-rmdir /S /Q phonegap-phonegap-2.2.0\lib\webos\framework
-mkdir phonegap-phonegap-2.2.0\lib\webos\framework
+rmdir /S /Q ..\phonegap-phonegap-2.2.0\lib\webos\framework
+mkdir ..\phonegap-phonegap-2.2.0\lib\webos\framework
 
-xcopy /E /Y /I api   phonegap-phonegap-2.2.0\lib\webos\framework\api
-xcopy /E /Y /I assets phonegap-phonegap-2.2.0\lib\webos\framework\assets
-xcopy /E /Y /I enyo   phonegap-phonegap-2.2.0\lib\webos\framework\enyo
-xcopy /E /Y /I lib    phonegap-phonegap-2.2.0\lib\webos\framework\lib
-xcopy /E /Y /I source phonegap-phonegap-2.2.0\lib\webos\framework\source
-copy appinfo.json  phonegap-phonegap-2.2.0\lib\webos\framework
-copy icon.png      phonegap-phonegap-2.2.0\lib\webos\framework
-copy debug.html    phonegap-phonegap-2.2.0\lib\webos\framework\index.html
-copy phonegap-phonegap-2.2.0\lib\webos\lib\cordova.webos.js phonegap-phonegap-2.2.0\lib\webos\framework\cordova-2.2.0.js
-call palm-package phonegap-phonegap-2.2.0\lib\webos\framework
-REM call palm-install *.ipk
+xcopy /E /Y /I ..\api    ..\phonegap-phonegap-2.2.0\lib\webos\framework\api
+xcopy /E /Y /I ..\assets ..\phonegap-phonegap-2.2.0\lib\webos\framework\assets
+xcopy /E /Y /I ..\enyo   ..\phonegap-phonegap-2.2.0\lib\webos\framework\enyo
+xcopy /E /Y /I ..\lib    ..\phonegap-phonegap-2.2.0\lib\webos\framework\lib
+xcopy /E /Y /I ..\source ..\phonegap-phonegap-2.2.0\lib\webos\framework\source
+
+copy ..\appinfo.json  ..\phonegap-phonegap-2.2.0\lib\webos\framework
+copy ..\icon.png      ..\phonegap-phonegap-2.2.0\lib\webos\framework
+copy ..\debug.html    ..\phonegap-phonegap-2.2.0\lib\webos\framework\index.html
+copy ..\phonegap-phonegap-2.2.0\lib\webos\lib\cordova.webos.js ..\phonegap-phonegap-2.2.0\lib\webos\framework\cordova-2.2.0.js
+call palm-package ..\phonegap-phonegap-2.2.0\lib\webos\framework
+call palm-install *.ipk
