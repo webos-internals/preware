db8586136f7b06fab42b5ba1d3159aff4445d601
Achim Königs <garfonso@tratschtante.de>
2013-11-10 14:01:07 +0100

    peacified jslint in App.js

--- source/App.js
+++ source/App.js
@@ -1,4 +1,5 @@
 // Preware App kind and main window.
+/*jslint sloppy: true */
 /*global enyo, onyx, preware, $L, navigator, device, PalmServiceBridge */
 
 //to reload changes on device: luna-send -n 1 palm://com.palm.applicationManager/rescan {}
@@ -7,45 +8,48 @@ enyo.kind({
 	name: "App",
 	layoutKind: "FittableRowsLayout",
 	components: [
-		{kind: "Signals",
-		onbackbutton: "handleBackGesture",
-		onCoreNaviDragStart: "handleCoreNaviDragStart",
-		onCoreNaviDrag: "handleCoreNaviDrag",
-		onCoreNaviDragFinish: "handleCoreNaviDragFinish"},
+		{
+			kind: "Signals",
+			onbackbutton: "handleBackGesture",
+			onCoreNaviDragStart: "handleCoreNaviDragStart",
+			onCoreNaviDrag: "handleCoreNaviDrag",
+			onCoreNaviDragFinish: "handleCoreNaviDragFinish"
+		},
 		{name: "AppPanels", kind: "AppPanels", fit: true},
 		{kind: "CoreNavi", fingerTracking: true},
 		{name: "SettingsDialog", kind: "SettingsDialog"},
 		{name: "ManageFeedsDialog", kind: "ManageFeedsDialog"},
-		{kind:"AppMenu", //onSelect: "appMenuItemSelected", 
+		{
+			kind: "AppMenu", //onSelect: "appMenuItemSelected", 
 			components: [
 				//{ content:"Install Package", ontap: "showInstallDialog" },
 				{ content: $L("Preferences"), ontap: "showSettingsDialog" },
 				{ content: $L("Manage Feeds"), ontap: "showManageFeedsDialog" }
 			]
-		}	
+		}
 	],
 	//Handlers
-	handleBackGesture: function(inSender, inEvent) {
+	handleBackGesture: function (inSender, inEvent) {
 		this.$.AppPanels.setIndex(0);
 		inEvent.preventDefault();
 	},
-	handleCoreNaviDragStart: function(inSender, inEvent) {
+	handleCoreNaviDragStart: function (inSender, inEvent) {
 		this.$.AppPanels.dragstartTransition(this.$.AppPanels.draggable === false ? this.reverseDrag(inEvent) : inEvent);
 	},
-	handleCoreNaviDrag: function(inSender, inEvent) {
+	handleCoreNaviDrag: function (inSender, inEvent) {
 		this.$.AppPanels.dragTransition(this.$.AppPanels.draggable === false ? this.reverseDrag(inEvent) : inEvent);
 	},
-	handleCoreNaviDragFinish: function(inSender, inEvent) {
+	handleCoreNaviDragFinish: function (inSender, inEvent) {
 		this.$.AppPanels.dragfinishTransition(this.$.AppPanels.draggable === false ? this.reverseDrag(inEvent) : inEvent);
 	},
-	showSettingsDialog: function(inSender, inEvent) {
+	showSettingsDialog: function (inSender, inEvent) {
 		this.$.SettingsDialog.show();
 	},
-	showManageFeedsDialog: function(inSender, inEvent) {
+	showManageFeedsDialog: function (inSender, inEvent) {
 		this.$.ManageFeedsDialog.show();
 	},
 	//Utility Functions
-	reverseDrag: function(inEvent) {
+	reverseDrag: function (inEvent) {
 		inEvent.dx = -inEvent.dx;
 		inEvent.ddx = -inEvent.ddx;
 		inEvent.xDirection = -inEvent.xDirection;
