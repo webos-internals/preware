53aeefd05f648d4592f5b25d23b999ab699ba6b2
Achim Königs <garfonso@tratschtante.de>
2013-11-10 15:13:35 +0100

    ManageFeedsDialog.js: peacified jslint

--- source/ManageFeedsDialog.js
+++ source/ManageFeedsDialog.js
@@ -1,4 +1,5 @@
-/*global enyo, onyx, preware, $L */
+/*jslint sloppy: true */
+/*global enyo, onyx, preware, $L, console */
 //shows a dialog that allows the user to change feed settings.
 
 //TODO: AppMenu can open over this PopUp.. urgs.. :(
@@ -18,38 +19,34 @@ enyo.kind({
 	scrimWhenModal: false,
 	components: [
 		{kind: "Signals", onLoadFeedsFinished: "onFeeds" },
-		{kind: "enyo.Scroller", components: [
-				horizontal: "hidden",
-				classes: "enyo-fill",
-				style: "background-image:url('assets/bg.png')",
-				touch: true,
-				fit: true,
-				components:[
-					{name: "CategoryRepeater", kind: "Repeater", onSetupItem: "setupCategoryItem", count: 0, components: [
-						{kind: "ListItem", content: "Category", ontap: "categoryTapped"}
-					]},
-			{kind: "onyx.Groupbox", components: [
-				{kind: "onyx.GroupboxHeader", content: $L("New Feed")},
-				//{kind: "enyo.FittableColumns", components: [
-					//{tag: "div", content: $L("Name"), fit: true},
+		{
+			kind: "enyo.Scroller",
+			horizontal: "hidden",
+			classes: "enyo-fill",
+			style: "background-image:url('assets/bg.png')",
+			touch: true,
+			fit: true,
+			components: [
+				{name: "CategoryRepeater", kind: "Repeater", onSetupItem: "setupCategoryItem", count: 0, components: [
+					{kind: "ListItem", content: "Category", ontap: "categoryTapped"}
+				]},
+				{kind: "onyx.Groupbox", components: [
+					{kind: "onyx.GroupboxHeader", content: $L("New Feed")},
 					{kind: "onyx.InputDecorator", components: [
 						{kind: "onyx.TextArea", name: "newFeedName", hint: $L("Name") }
 					]},
-				//]},
-				//{kind: "enyo.FittableColumns", components: [
-					//{tag: "div", content: $L("URL"), fit: true},
 					{kind: "onyx.InputDecorator", components: [
 						{kind: "onyx.TextArea", name: "newFeedURL", hint: $L("URL"), content: "http://" }
 					]},
-				//]},
-				{kind: "enyo.FittableColumns", components: [
-					{tag: "div", content: $L("Compressed"), fit: true},
-					{kind: "onyx.ToggleButton", name: "newFeedCompressedToggle"}
-				]},
-				{kind: "onyx.Button", style: "margin:5px;font-size:24px;float:center;", content: $L("Add Feed"), ontap: "addNewFeed"}
-			]}, //end of action group
-			{kind: "onyx.Button", style: "margin:5px;font-size:24px;float:center;", content: $L("Close"), ontap: "closePopup"}
-		]}
+					{kind: "enyo.FittableColumns", components: [
+						{tag: "div", content: $L("Compressed"), fit: true},
+						{kind: "onyx.ToggleButton", name: "newFeedCompressedToggle"}
+					]},
+					{kind: "onyx.Button", style: "margin:5px;font-size:24px;float:center;", content: $L("Add Feed"), ontap: "addNewFeed"}
+				]}, //end of action group
+				{kind: "onyx.Button", style: "margin:5px;font-size:24px;float:center;", content: $L("Close"), ontap: "closePopup"}
+			]
+		}
 	],
 	create: function (inSender, inEvent) {
 		this.inherited(arguments);
